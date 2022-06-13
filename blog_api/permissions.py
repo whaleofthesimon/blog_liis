488977f9abc30e3d59260c012749e19990cebf2b
whaleofthesimon <snv.whale@gmail.com>
2022-06-13 21:13:38 +0300

    Deleted excess permissions + few optimizations for other

--- blog_api/permissions.py
+++ blog_api/permissions.py
@@ -1,28 +1,15 @@
 from rest_framework import permissions
 
 
-class IsNonAuthenticated(permissions.BasePermission):
-    message = 'Необходимо выйти из текущего аккаунта, прежде чем приступить к регистрации нового'
-
-    def has_permission(self, request, view):
-        return bool(not request.user.is_authenticated)
-
-
 class IsAuthor(permissions.BasePermission):
     def has_permission(self, request, view):
-        try:
-            return bool(request.user.is_author)
-        except AttributeError:
-            return False
+        if hasattr(request.user, 'user_role'):
+            return request.user.user_role == 'author'
 
 
 class IsCurrentAuthorUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user.is_authenticated)
+        return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        try:
-            print(request.user, obj.author)
-            return bool(request.user == obj.author)
-        except AttributeError:
-            return False
+        return request.user == obj.author
