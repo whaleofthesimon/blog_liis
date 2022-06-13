83ea690c26936a22e2ff61d5f7676ad98a81b221
whaleofthesimon <snv.whale@gmail.com>
2022-06-13 21:14:32 +0300

    Deleted excess serializers + optimization

--- blog_api/serializers.py
+++ blog_api/serializers.py
@@ -1,48 +1,31 @@
 from django.contrib.auth import get_user_model
 import django.contrib.auth.password_validation as validators
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
-from blog_api.models import Article, CustomUser
-from blog_api.validators import FullCommonValidator
 
-UserModel = get_user_model()
+from blog_api.models import Article
+
+User = get_user_model()
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = UserModel
+        model = User
         fields = ('id', 'email', 'password')
         write_only_fields = ('password',)
         read_only_fields = ('id',)
 
     def validate_password(self, value):
-        try:
-            password_validators = [FullCommonValidator(), ]
-            validators.validate_password(value, password_validators=password_validators)
-        except ValidationError as exc:
-            raise serializers.ValidationError(str(exc))
+        validators.validate_password(value)
         return value
 
     def create(self, validated_data):
-        user = UserModel.objects.create(email=validated_data['email'])
+        user = User.objects.create(email=validated_data['email'])
         user.set_password(validated_data['password'])
         user.save()
         return user
 
 
-class TimelineSerializer(serializers.ModelSerializer):
+class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = '__all__'
-
-
-class ArticleCreationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = '__all__'
-
-
-class UserAdministrationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ('id', 'email', 'is_superuser', 'is_author')
+        fields = ('topic', 'filling', 'author', 'access')
