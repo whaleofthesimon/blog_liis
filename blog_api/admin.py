b50ad15864af13b3eb6b1e68717ffd35dc7186a7
whaleofthesimon <snv.whale@gmail.com>
2022-06-13 21:09:47 +0300

    Added model's registrations in admin panel

--- blog_api/admin.py
+++ blog_api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from blog_api.models import CustomUser
+from blog_api.models import CustomUser, Article
 
-
+admin.site.register(CustomUser)
+admin.site.register(Article)
